cb557f0b1f970e33f1da344918437b06c3cb8f72
trantoan11011996 <trantoan11011996@gmail.com>
2022-06-17 18:08:50 +0700

    setup_package

---
+++ clothing_shop/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { ListGroup } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <ListGroup.Item>
+        hiiii
+      </ListGroup.Item>
+      <ListGroup.Item>
+        hiiii
+      </ListGroup.Item>
+      <ListGroup.Item>
+        hiiii
+      </ListGroup.Item>
+    </div>
+  );
+}
+
+export default App;
